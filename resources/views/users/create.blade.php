9ad1f66e6bb8295d6b8db94a1fdfecfe539db897
Jose Posada <josedanielpm@gmail.com>
2019-06-21 08:29:10 -0500

    bootstrap 4

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -4,40 +4,46 @@
 
 @section('content')
 
-    <h1>Create user</h1>
-
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <h6>Please correct the errors:</h6>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+    <div class="card">
+        <div class="card-header">
+            <h4>Create user</h4>
         </div>
-    @endif
+        <div class="card-body">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <h6>Please correct the errors:</h6>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        
+            <form method="POST" action="{{ url('users') }}">
+                @csrf
+        
+                <div class="form-group">
+                    <label for="name">Name:</label>
+                    <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}">
+                </div>
+        
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" class="form-control" name="email" id="email" value="{{ old('email') }}">
+                </div>
+        
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" class="form-control" name="password" id="password">
+                </div>
+        
+                <a class="btn btn-secondary" href="{{ route('users.index') }}">Back</a>
+                <button type="submit" class="btn btn-primary">Create</button>
+            </form>
+        </div>
+    </div>
 
-    <form method="POST" action="{{ url('users') }}">
-        @csrf
-
-        <label for="name">Name:</label>
-        <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}">
-
-
-        <label for="email">Email:</label>
-        <input type="email" class="form-control" name="email" id="email" value="{{ old('email') }}">
-
-        <label for="password">Password:</label>
-        <input type="password" class="form-control" name="password" id="password">
-
-        <button type="submit" class="btn btn-primary">Create</button>
-
-    </form>
-
-
-    <p>
-        <a href="{{ route('users.index') }}">Back</a>
-        {{-- <a href="{{ action('UserController@index') }}">Back to users list</a> --}}
-    </p>
+    {{-- <a href="{{ action('UserController@index') }}">Back to users list</a> --}}
     
 @endsection
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -4,43 +4,46 @@
 
 @section('content')
 
-    <h1>Edit user</h1>
 
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <h6>Please correct the errors:</h6>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+    <div class="card">
+        <div class="card-header">
+            <h4>Edit user</h4>
         </div>
-    @endif
-
-    <form method="POST" action="{{ url("users/{$user->id}") }}">
-        @method('PUT')
-        @csrf
-
-        <label for="name">Name:</label>
-        <input type="text" class="form-control" name="name" id="name" value="{{ old('name', $user->name) }}">
-
-
-        <label for="email">Email:</label>
-        <input type="email" class="form-control" name="email" id="email" value="{{ old('email', $user->email) }}">
-
-
-        <label for="password">Password:</label>
-        <input type="password" class="form-control" name="password" id="password">
-
-
-        <button type="submit" class="btn btn-primary">Update</button>
-
-    </form>
-
-
-    <p>
-        <a href="{{ route('users.index') }}">Back</a>
-        {{-- <a href="{{ action('UserController@index') }}">Back to users list</a> --}}
-    </p>
+        <div class="card-body">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <h6>Please correct the errors:</h6>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        
+            <form method="POST" action="{{ url("users/{$user->id}") }}">
+                @method('PUT')
+                @csrf
+        
+                <div class="form-group">
+                    <label for="name">Name:</label>
+                    <input type="text" class="form-control" name="name" id="name" value="{{ old('name', $user->name) }}">
+                </div>
+        
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" class="form-control" name="email" id="email" value="{{ old('email', $user->email) }}">                
+                </div>
+        
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" class="form-control" name="password" id="password">
+                </div>
+        
+                <a class="btn btn-secondary" href="{{ route('users.index') }}">Back</a>
+                <button type="submit" class="btn btn-primary">Update</button>
+            </form>
+        </div>
+    </div>
     
 @endsection
